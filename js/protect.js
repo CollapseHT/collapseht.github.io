4a304306cfaf61a85740ead2c8f21f426cc7c082
CollapseHT <a01688958225@gmail.com>
2024-06-03 09:07:12 +0800

    Update protect.js

--- js/protect.js
+++ js/protect.js
@@ -4,15 +4,17 @@ function hashPassword(password) {
 
 function checkPassword() {
     var password = document.getElementById('password').value;
-    var correctHash = 'TGlu';
+    let correctHash = ['TGlu', 'Q0hU'];
 
-    if (hashPassword(password) === correctHash) {
-        document.getElementById('login-form').style.display = 'none';
-        document.getElementById('pctpwd').style.display = 'block';
-        localStorage.setItem('authenticated', 'true');
-    } else {
-        alert('你/妳無權瀏覽此頁面');
+    for (i = 0; i < correctHash.length; i++) {
+        if (hashPassword(password) === correctHash[i]) {
+            document.getElementById('login-form').style.display = 'none';
+            document.getElementById('pctpwd').style.display = 'block';
+            localStorage.setItem('authenticated', 'true');
+            return;
+        }
     }
+    alert('你/妳無權瀏覽此頁面');
 }
 
 window.onload = function () {
